aee668d30a5d0885d9266cc677b05f3c7f0b5754
Takumi Ito <takumi10194617@gmail.com>
2016-10-30 01:26:37 +0900

    Delete unnecessary assets
    # Conflicts:
    #	Assets/UniWeb.meta

---
+++ Assets/MyAssets/Scripts/RasPiAddressInputScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RasPiAddressInputScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (PlayerPrefs.HasKey("rasPiAddress")){
+			GetComponent<InputField>().text = PlayerPrefs.GetString("rasPiAddress");
+			Debug.Log("Loaded, " + PlayerPrefs.GetString("rasPiAddress"));
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
